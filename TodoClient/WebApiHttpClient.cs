200a7aba2b391be841b07ebcc4194eff41741ec5
Dany Abplanalp <dany.abplanalp@duagon.com>
2017-05-18 16:44:52 +0200

    Naming convention

--- TodoClient/WebApiHttpClient.cs
+++ TodoClient/WebApiHttpClient.cs
@@ -9,33 +9,33 @@ namespace TodoClient
 {
     public class WebApiHttpClient<TEntity, TKey> : IWebApiClient<TEntity, TKey> where TEntity : IEntityWithKey<TKey>
     {
-        protected readonly HttpClient _client = new HttpClient();
-        protected readonly string _url;
+        protected readonly HttpClient Client = new HttpClient();
+        protected readonly string BaseUrl;
 
         public WebApiHttpClient(string baseUrl)
         {
-            _url = baseUrl;
+            BaseUrl = baseUrl;
         }
 
-        protected string Url() => _url;
-        protected string Url(TKey id) => $"{_url}/{id}";
+        protected string Url() => BaseUrl;
+        protected string Url(TKey id) => $"{BaseUrl}/{id}";
 
         public async Task<IEnumerable<TEntity>> GetAll()
-            => Deserialize<List<TEntity>>(await _client.GetStringAsync(Url()));
+            => Deserialize<List<TEntity>>(await Client.GetStringAsync(Url()));
 
         public async Task<TEntity> Find(TKey id)
-            => Deserialize<TEntity>(await _client.GetStringAsync(Url(id)));
+            => Deserialize<TEntity>(await Client.GetStringAsync(Url(id)));
 
         public async Task<bool> Remove(TKey id)
-            => (await _client.DeleteAsync(Url(id))).IsSuccessStatusCode;
+            => (await Client.DeleteAsync(Url(id))).IsSuccessStatusCode;
 
         public async Task<bool> Update(TKey id, TEntity todo)
-            => (await _client.PutAsync(Url(id), CreateJsonContent(todo))).IsSuccessStatusCode;
+            => (await Client.PutAsync(Url(id), CreateJsonContent(todo))).IsSuccessStatusCode;
 
         /// <exception cref="HttpRequestException">If the IsSuccessStatusCode property for the HTTP response is false</exception>
         public async Task<Uri> Add(TEntity todo)
         {
-            var response = await _client.PostAsync(Url(), CreateJsonContent(todo));
+            var response = await Client.PostAsync(Url(), CreateJsonContent(todo));
             response.EnsureSuccessStatusCode();
 
             todo.Key = Deserialize<TEntity>(await response.Content.ReadAsStringAsync()).Key;
